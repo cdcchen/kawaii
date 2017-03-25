e23a581be91e02d19c7417ae7c4637ef222cd5b4
cdcchen <cdcchen@gmail.com>
2017-03-25 11:51:14 +0800

    Add WebsocketServer base class

---
+++ src/server/WebsocketServer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chendong
+ * Date: 2017/3/24
+ * Time: 15:03
+ */
+
+namespace kawaii\server;
+
+
+use kawaii\base\Base;
+use kawaii\base\ServerListener;
+
+class WebsocketServer extends Base
+{
+
+    protected function bindCallback()
+    {
+        // TODO: Implement bindCallback() method.
+    }
+
+    static protected function createSwooleServer(ServerListener $listener)
+    {
+        // TODO: Implement createSwooleServer() method.
+    }
+}
