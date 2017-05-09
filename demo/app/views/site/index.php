d341a67cdb5ebc59921e6045956bc2952fb1fb46
cdcchen <cdcchen@gmail.com>
2017-05-09 21:09:39 +0800

    add bootstrap class

--- demo/app/views/site/index.php
+++ demo/app/views/site/index.php
@@ -10,19 +10,21 @@
 
 <div id="app">
   <form class="form-inline text-center my-2">
-    <select v-model="project">
-      <option value="">所有项目</option>
-      <option value="webappp">webapp</option>
-      <option value="exam">exam</option>
-      <option value="console">console</option>
-      <option value="backend">backend</option>
-      <option value="openapi">openapi</option>
-      <option value="console">console</option>
-      <option value="paper">paper</option>
-      <option value="passport">passport</option>
-      <option value="school_manage">school-manage</option>
-      <option value="wechat">wechat</option>
-    </select>
+    <div class="form-group">
+      <select v-model="project" class="form-control">
+        <option value="">所有项目</option>
+        <option value="webappp">webapp</option>
+        <option value="exam">exam</option>
+        <option value="console">console</option>
+        <option value="backend">backend</option>
+        <option value="openapi">openapi</option>
+        <option value="console">console</option>
+        <option value="paper">paper</option>
+        <option value="passport">passport</option>
+        <option value="school_manage">school-manage</option>
+        <option value="wechat">wechat</option>
+      </select>
+    </div>
     <button type="button" class="btn btn-primary" @click="changeState">{{btnLabel}}</button>
     <button type="button" class="btn btn-outline-info" @click="clear">清除内容</button>
     <button type="button" class="btn btn-outline-info" @click="send">发送内容</button>
