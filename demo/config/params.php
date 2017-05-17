047b437b649787c40d318a2121ce316f8de97f43
cdcchen <cdcchen@gmail.com>
2017-05-17 11:34:54 +0800

    修改demo项目的websocket地址

--- demo/config/params.php
+++ demo/config/params.php
@@ -8,7 +8,7 @@
 
 return [
     'email' => 'admin@cdc.com',
-    'ws_host' => KAWAII_ENV_PROD ? 'ws://114.55.130.163:9527/log' : 'ws://127.0.0.1:9527/log',
+    'ws_host' => KAWAII_ENV_PROD ? 'ws://log.mschool.cn:9527/log' : 'ws://127.0.0.1:9527/log',
     'redis_host' => KAWAII_ENV_PROD ? '2dbf2b5dbcc6435f.redis.rds.aliyuncs.com' : '127.0.0.1',
     'redis_password' => KAWAII_ENV_PROD ? 'yDb123321' : null,
 ];
